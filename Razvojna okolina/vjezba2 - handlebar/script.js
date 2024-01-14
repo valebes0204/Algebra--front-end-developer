0c8f53a28f223c465b27b2d6a8ac05a1a3bccdfa
valebes0204 <besen.valentina1@gmail.com>
2024-01-14 19:52:31 +0100

    vjezba2

---
+++ Razvojna okolina/vjezba2 - handlebar/script.js
@@ -0,0 +1,17 @@
+Handlebars.registerHelper("indexPlusJedan", function (options) {
+  return options.data.index + 1;
+});
+
+fetch("https://pokeapi.co/api/v2/pokemon-color/yellow")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    const source = document.getElementById("pokemon-list").innerHTML;
+    const template = Handlebars.compile(source);
+    const context = { pokemon: data.pokemon_species };
+    const html = template(context);
+    document.getElementById("result").innerHTML = html;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
